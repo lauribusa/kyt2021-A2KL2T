0e7dd243eb7963435a621b7e6e722c598eb35fc0
LAPTOP-5QR69DH2\Lauri <lauribusa>
2021-10-09 12:51:35 +0200

    Update ballController to use normalized values

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,5 +1,11 @@
 using UnityEngine;
 
+public enum VelocityCollisionMode
+{
+    Increment,
+    Decrement,
+};
+
 [RequireComponent(typeof(Rigidbody2D))]
  public class BallController : MonoBehaviour
 {
@@ -8,12 +14,18 @@ using UnityEngine;
     public CircleCollider2D ballCollider;
     public Rigidbody2D ballRigidbody;
     public Vector2 initialImpulse;
+    [Tooltip("Velocity should increment or decrement on collision")]
+    public VelocityCollisionMode velocityCollisionMode;
+    [Header("Velocity on collision")]
+    [Tooltip("Increment velocity by this amount on collision")]
+    public float velocityIncrement;
+    [Tooltip("Decrement velocity by this amount on collision")]
+    public float velocityDecrement;
     #endregion
 
 
     #region Private And Protected
     private float ballVelocityMagnitude { get; set; }
-    private Vector2 preHitVelocity { get; set; }
 
     #endregion
 	
@@ -21,31 +33,53 @@ using UnityEngine;
 	
     private void Start()
     {
-        ballCollider.attachedRigidbody.AddForce(initialImpulse, ForceMode2D.Force);
+        ballRigidbody.AddForce(initialImpulse, ForceMode2D.Impulse);
         ballVelocityMagnitude = ballCollider.attachedRigidbody.velocity.magnitude;
-        ballRigidbody = ballCollider.attachedRigidbody;
     }
 
     private void FixedUpdate()
     {
-        preHitVelocity = ballRigidbody.velocity;
+        ballRigidbody.velocity = ballRigidbody.velocity.normalized * ballVelocityMagnitude;
     }
 
     #endregion
 
-    private void OnCollisionStay2D(Collision2D other)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        Vector2 avgNormal = Vector2.zero;
-        for (int i = 0; i < other.contactCount; i++)
+        if(velocityCollisionMode == VelocityCollisionMode.Increment)
         {
-            avgNormal += other.GetContact(i).normal;
+            IncreaseVelocityOnCollision();
+        } else
+        {
+            DecreaseVelocityOnCollision();   
         }
-        avgNormal /= other.contactCount;
-        ballRigidbody.velocity = Vector2.Reflect(preHitVelocity, avgNormal);
     }
 
-
     #region Main
+
+    private void IncreaseVelocityOnCollision()
+    {
+        ballVelocityMagnitude += velocityIncrement;
+    }
+
+    private void DecreaseVelocityOnCollision()
+    {
+        ballVelocityMagnitude -= velocityDecrement;
+    }
+
+    private void ReflectBall(Collision2D collision)
+    {
+        ballVelocityMagnitude += 1;
+        // KEEP FOR PLAYER REFLECT FUNCTION
+        //Vector2 avgNormal = Vector2.zero;
+        //for (int i = 0; i < collision.contactCount; i++)
+        //{
+        //    avgNormal += collision.GetContact(i).normal;
+        //}
+        //avgNormal /= collision.contactCount;
+        //ballRigidbody.velocity = Vector2.Reflect(preHitVelocity, avgNormal);
+        //ballRigidbody.velocity = ballRigidbody.velocity.normalized * ballVelocityMagnitude;
+    }
 
     #endregion
 
